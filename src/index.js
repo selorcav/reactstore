b084547c61a11cee10e54a60192299b7848d9c0e
selorcav <se.lorcav@gmail.com>
2021-07-26 00:48:14 -0400

    navbar creado

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeProvider, createTheme  } from '@material-ui/core/styles';
+
+// import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0a0506',
+    },
+    secondary: {
+      main: '#dc232e',
+    },
+  },
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme ={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +30,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
